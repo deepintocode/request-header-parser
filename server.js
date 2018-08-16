c3c2513e22f5f936bba6b9c50f28e0c6a3ccca1b
Teo <todorare@gmail.com>
2018-08-16 10:10:07 +0100

    Check if IPv6 or IPv4 address

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/whoami', (req, res) => {
+  let ip = req.ip;
+  if (ip.substr(0, 7) == "::ffff:") {
+    ip = ip.substr(7)
+  }
   res.json({
-    "ipaddress": req.ip,
+    "ipaddress": ip,
     "language": req.headers['accept-language'],
     "software": req.headers['user-agent']
   });
