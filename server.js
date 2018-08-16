3cf9729b800434bd3ce22229fc5466bfdd539221
Teo <todorare@gmail.com>
2018-08-16 09:52:25 +0100

    Fix route end point

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  // res.json({
-  //   "ipaddress": req.ip,
-  //   "language": req.acceptsLanguages().join(',')
-  // })
-  res.send(req.headers)
+  res.send('A request header parser microservice');
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('The server is listening at port 3000'));
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    "ipaddress": req.ip,
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent']
+  });
+});
+
+app.listen(process.env.PORT | 3000, () => console.log('The server is listening at port 3000'));
